5e4d907b70c484510a91a113985c1e5c8b4268fa
alyssaschilke <arschilke@loyola.edu>
2023-05-10 21:42:29 -0400

    fix comments

--- src/views/CustomersCreate.vue
+++ src/views/CustomersCreate.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+/*
 import { ref } from "vue";
 import { Customer } from "../webApiClient";
 
-/*
+
 const customer = ref<Customer>({ joinDate: Date.now });
 
 
@@ -29,7 +30,7 @@ function isValidSSN(value: string) {
 <template>
   <div class="container">
     <h1>Create New Customer</h1>
-    <form @submit.prevent="handleSubmit">
+    <form><!--@submit.prevent="handleSubmit">-->
       <label class="form-label">Customer number</label>
       <input type="number" min="10000" max="99999" class="form-control" />
       <label class="form-label">First Name</label>
@@ -45,47 +46,21 @@ function isValidSSN(value: string) {
       <h6>Primary address</h6>
       <div>
         <label class="form-label" for="street-address">Street address</label>
-        <input
-          type="text"
-          id="street-address"
-          name="street-address"
-          class="form-control"
-          autocomplete="street-address"
-          required
-        />
+        <input type="text" id="street-address" name="street-address" class="form-control" autocomplete="street-address"
+          required />
       </div>
       <div>
-        <label for="postal-code" class="form-label"
-          >ZIP or postal code (optional)</label
-        >
-        <input
-          id="postal-code"
-          class="form-control"
-          name="postal-code"
-          autocomplete="postal-code"
-        />
+        <label for="postal-code" class="form-label">ZIP or postal code (optional)</label>
+        <input id="postal-code" class="form-control" name="postal-code" autocomplete="postal-code" />
       </div>
       <div>
         <label for="city" class="form-label">City</label>
-        <input
-          required
-          type="text"
-          id="city"
-          class="form-control"
-          name="city"
-          autocomplete="address-level2"
-          enterkeyhint="next"
-        />
+        <input required type="text" id="city" class="form-control" name="city" autocomplete="address-level2"
+          enterkeyhint="next" />
       </div>
       <div>
         <label for="country" class="form-label">Country or region</label>
-        <input
-          id="country"
-          name="country"
-          class="form-select"
-          autocomplete="country"
-          required
-        />
+        <input id="country" name="country" class="form-select" autocomplete="country" required />
       </div>
       <label class="form-label">Mobile phone number</label>
       <input type="phone" class="form-control" />
